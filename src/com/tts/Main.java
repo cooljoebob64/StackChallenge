9f3da33aa71f50f57eb76d6ac76e82e36cdbb8f9
Joshua Luppes <cooljoebob64@gmail.com>
2021-01-15 15:50:48 -0600

    Adds Readme file explaining challenge parameters
    Also adds some formatting fixes

--- src/com/tts/Main.java
+++ src/com/tts/Main.java
@@ -5,10 +5,6 @@ import java.util.Stack;
 public class Main {
 
     public static void main(String[] args) {
-        boolean myBool = true;
-
-//        System.out.println(myBool);
-
         String input = """
                 ({}[])
                 (({()})))
@@ -24,31 +20,36 @@ public class Main {
                 """;
 
         String[] inputList = input.split("\n");
-        for (String line:inputList){
-
+        for (String line : inputList) {
 
             Stack<Character> myStack = new Stack<>();
             boolean balanced = true;
 
-            for(int i=0; i<line.length();i++){
+            for (int i = 0; i < line.length(); i++) {
                 char currentChar = line.charAt(i);
-//                System.out.println("Current char: " + currentChar);
-                char prevChar ='a';
-                if(currentChar=='{'||currentChar=='('||currentChar=='[')
+//                System.out.println("Current char: " + currentChar); // For debugging
+                char prevChar = 'a';
+                if (currentChar == '{' || currentChar == '(' || currentChar == '[')
                     myStack.push(currentChar);
-                else if(!(myStack.isEmpty())) prevChar = myStack.pop();
-//                System.out.println("Previous char: " + prevChar);
+                else if (!(myStack.isEmpty())) prevChar = myStack.pop();
+//                System.out.println("Previous char: " + prevChar); // For debugging
 
-                switch (currentChar){
-                    case '}':if (!(prevChar=='{')) balanced=false; break;
-                    case ')':if (!(prevChar=='('))balanced=false; break;
-                    case ']':if (!(prevChar=='['))balanced=false; break;
-//                    default: balanced=false;
+                switch (currentChar) {
+                    case '}':
+                        if (!(prevChar == '{')) balanced = false;
+                        break;
+                    case ')':
+                        if (!(prevChar == '(')) balanced = false;
+                        break;
+                    case ']':
+                        if (!(prevChar == '[')) balanced = false;
+                        break;
                 }
             }
             if (!(myStack.isEmpty()))
                 balanced = false;
 
             System.out.println(balanced);
-        }    }
+        }
+    }
 }
